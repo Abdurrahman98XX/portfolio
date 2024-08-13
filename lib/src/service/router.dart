ab991d376cdef2c28bcaadf1b58c1583417f9d93
Abdurrahman Adel <abduadel.pm@gmail.com>
2024-08-13 04:52:01 +0300

    fuck flutter

--- lib/src/service/router.dart
+++ lib/src/service/router.dart
@@ -1,27 +1,27 @@
 part of 'service_locator.dart';
 
-GoRouter get _router => GoRouter(
-      redirectLimit: 0,
-      debugLogDiagnostics: true,
-      restorationScopeId: Const.id,
-      navigatorKey: Const.navigatorKey,
-      observers: [TalkerRouteObserver(ServiceLocator.logger)],
-      routes: [
-        GoRoute(
-          path: '/',
-          builder: (context, state) {
-            return Scaffold(
-              appBar: AppBar(
-                title: const Text('Home'),
-              ),
-              body: Center(
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('About'),
-                ),
-              ),
-            );
-          },
-        ),
-      ],
-    );
+final _router = GoRouter(
+  redirectLimit: 0,
+  debugLogDiagnostics: true,
+  restorationScopeId: Const.id,
+  navigatorKey: Const.navigatorKey,
+  observers: [TalkerRouteObserver(ServiceLocator.logger)],
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Home'),
+          ),
+          body: Center(
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text('About'),
+            ),
+          ),
+        );
+      },
+    ),
+  ],
+);
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import appkit_ui_element_colors
 import firebase_core
+import macos_ui
+import macos_window_utils
 import path_provider_foundation
 import screen_retriever
 import share_plus
@@ -15,7 +18,10 @@ import url_launcher_macos
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppkitUiElementColorsPlugin.register(with: registry.registrar(forPlugin: "AppkitUiElementColorsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  MacOSUiPlugin.register(with: registry.registrar(forPlugin: "MacOSUiPlugin"))
+  MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
